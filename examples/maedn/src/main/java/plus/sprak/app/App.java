2ca24024e84569068047e03ac62aab246414724e
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2025-11-12 23:31:24 +0100

    Add very basic structure for application

--- examples/maedn/src/main/java/plus/sprak/app/App.java
+++ examples/maedn/src/main/java/plus/sprak/app/App.java
@@ -1,9 +1,25 @@
 package plus.sprak.app;
 
 import plus.jboard.core.GameApplication;
+import plus.jboard.core.Piece;
 
-public class App extends GameApplication {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        BufferedImage knight = ImageIO.read(new File(Path.of("src", "main", "resources", "knight.png").toUri()));
+        GameApplication app = new GameApplication("Mensch-ärgere-dich-nicht!");
+
+        for(int i = 0; i < 10; i++) {
+            Piece p = new Piece(knight);
+            p.getTransform().translate(i * 20, i * 20);
+            app.addPiece(p);
+        }
+
+        app.run();
     }
 }
--- jboard/src/main/java/plus/jboard/core/GameApplication.java
+++ jboard/src/main/java/plus/jboard/core/GameApplication.java
@@ -1,4 +1,20 @@
 package plus.jboard.core;
 
 public class GameApplication {
+
+    private String title;
+    private GameWindow window;
+
+    public GameApplication(String title) {
+        this.title = title;
+        window = new GameWindow(title);
+    }
+
+    public void run() {
+    }
+
+    public void addPiece(Piece p) {
+        window.renderContext.addPiece(p);
+    }
+
 }
