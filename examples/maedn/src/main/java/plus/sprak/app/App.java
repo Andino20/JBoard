d8cd85da1bef2f4a1a567332055aa3f6e2064d37
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2025-11-13 11:13:02 +0100

    Simplify main

--- examples/maedn/src/main/java/plus/sprak/app/App.java
+++ examples/maedn/src/main/java/plus/sprak/app/App.java
@@ -1,25 +1,42 @@
 package plus.sprak.app;
 
-import plus.jboard.core.GameApplication;
-import plus.jboard.core.Piece;
-
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        BufferedImage knight = ImageIO.read(new File(Path.of("src", "main", "resources", "knight.png").toUri()));
-        GameApplication app = new GameApplication("Mensch-ärgere-dich-nicht!");
 
-        for(int i = 0; i < 10; i++) {
-            Piece p = new Piece(knight);
-            p.getTransform().translate(i * 20, i * 20);
-            app.addPiece(p);
+    public static class RenderContext extends JPanel {
+
+        public RenderContext() {
+            this.setBackground(Color.WHITE);
         }
 
-        app.run();
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame("Mensch-ärgere-dich-nicht!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(720, 480);
+        frame.setLocationRelativeTo(null);
+
+        RenderContext renderContext = new RenderContext();
+        frame.add(renderContext);
+
+        frame.setVisible(true);
+
+        BufferedImage piece = ImageIO.read(new File(Path.of("src", "main", "resources", "piece.png").toUri()));
+        for(int i = 0; i < 10; i++) {
+
+        }
     }
 }
