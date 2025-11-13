01f45158eb59f17016b9f55a7db1977cba5fc0e7
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2025-11-13 13:47:25 +0100

    Very simple demo of placing pieces and moving them

--- examples/maedn/src/main/java/plus/sprak/app/App.java
+++ examples/maedn/src/main/java/plus/sprak/app/App.java
@@ -7,10 +7,24 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.TimerTask;
+import java.util.stream.IntStream;
+
 
 public class App {
 
+    public record GameObject(BufferedImage image, int x, int y) {
+        public GameObject(BufferedImage image, int x, int y) {
+            this.image = image;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
     public static class RenderContext extends JPanel {
+
+        private List<GameObject> objects;
 
         public RenderContext() {
             this.setBackground(Color.WHITE);
@@ -19,8 +33,15 @@ public class App {
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            for (GameObject object : objects) {
+                g2d.drawImage(object.image, object.x, object.y, null);
+            }
         }
 
+        public void setGameObjects(List<GameObject> objects) {
+            this.objects = objects;
+        }
     }
 
     public static void main(String[] args) throws IOException {
@@ -35,8 +56,25 @@ public class App {
         frame.setVisible(true);
 
         BufferedImage piece = ImageIO.read(new File(Path.of("src", "main", "resources", "piece.png").toUri()));
-        for(int i = 0; i < 10; i++) {
+        List<GameObject> pieces1 = IntStream.range(0, 10)
+                .mapToObj(p -> new GameObject(piece, p * 64, p * 64))
+                .toList();
+        List<GameObject> pieces2 = IntStream.range(0, 10)
+                .mapToObj(p -> new GameObject(piece, p * 64, 10*64 - p * 64))
+                .toList();
+        renderContext.setGameObjects(pieces1);
 
-        }
+        java.util.Timer t = new java.util.Timer();
+        t.scheduleAtFixedRate(new TimerTask() {
+            List<GameObject> next = pieces2;
+
+            @Override
+            public void run() {
+                renderContext.setGameObjects(next);
+                next = (next == pieces1) ? pieces2 : pieces1;
+                renderContext.repaint();
+            }
+        }, 0, 1500);
+
     }
 }
