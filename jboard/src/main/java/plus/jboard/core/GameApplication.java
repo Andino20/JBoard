c0daa4b2e9ecb29eb2cd7e2385498fa2bb9ca5e4
Schmidt Linda <linda.schmidt@stud.sbg.ac.at>
2025-11-15 12:42:15 +0100

    Gameloop and rendering

--- jboard/src/main/java/plus/jboard/core/GameApplication.java
+++ jboard/src/main/java/plus/jboard/core/GameApplication.java
@@ -1,11 +1,16 @@
 package plus.jboard.core;
 
 import plus.jboard.render.Drawable;
+import plus.jboard.render.RenderContext;
 
 import javax.swing.*;
 import java.util.List;
 
 public class GameApplication {
+    private final RenderContext renderer;
+    private final List<? extends Drawable> objects;
+    private static final double FPS = 60.0;
+    private static final double FRAME_TIME = 1_000_000_000 / FPS;
 
     public GameApplication(String title, int width, int height, List<? extends Drawable> gameObjects) {
         JFrame frame = new JFrame(title);
@@ -13,6 +18,27 @@ public class GameApplication {
         frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        renderer = new RenderContext();
+        frame.add(renderer);
+        objects = gameObjects;
+    }
+
+    public void run() {
+
+        long lastTime = System.nanoTime();
+        double delta = 0;
+
+        while (true) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / FRAME_TIME;
+            lastTime = now;
+
+            while (delta >= 1) {
+                renderer.render(objects);
+                delta--;
+            }
+        }
+
     }
 
 }
