8de5d005458af0fbc94b4191e2b2e4958a0cc7df
Schmidt Linda <linda.schmidt@stud.sbg.ac.at>
2025-12-13 23:46:03 +0100

    Semi funktionales Spiel

--- jboard/src/main/java/plus/jboard/core/GameObject.java
+++ jboard/src/main/java/plus/jboard/core/GameObject.java
@@ -8,7 +8,7 @@ import plus.jboard.render.SpriteRenderObject;
 
 import java.awt.Graphics2D;
 
-public abstract class GameObject implements RenderObject {
+public class GameObject implements Drawable {
     private Vector2D position;
     protected Sprite sprite;
 
